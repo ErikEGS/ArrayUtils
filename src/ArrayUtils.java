6de8a377dc98542365036b826f90f7ef59dc07ae
ErikEGS <erik.harutyunyan.work@gmail.com>
2021-12-05 01:52:04 +0400

    Update

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,154 +1,165 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class ArrayUtils {
+ public final class ArrayUtils {
 
-  /* task1 Create a method that sorts the int array elements by odd and even number such
-           that if first number is odd then second must be even number and so on for the next
-           elements of the array.  */
+   private final int [] arr;
 
-  static void arrangeOddAndEven(int[] arr) {
+   public ArrayUtils () {
+     this.arr = new int[0];
+   }
 
-    Arrays.sort(arr);
+   public ArrayUtils (int[] arr) {
+     if (arr == null)
+       this.arr = null;
+     else {
+       this.arr = new int[arr.length];
+       System.arraycopy(arr,0,this.arr,0,arr.length);
+     }
+   }
 
-    int oddInd = 1;
-    int evenInd = 0;
-    while (true) {
-      while (evenInd < arr.length && arr[evenInd] % 2 == 0) {
-        evenInd += 2;
-      }
-      while (oddInd < arr.length && arr[oddInd] % 2 == 1) {
-        oddInd += 2;
-      }
-      if (evenInd < arr.length && oddInd < arr.length) {
-        int temp = arr[evenInd];
-        arr[evenInd] = arr[oddInd];
-        arr[oddInd] = temp;
-      } else {
-        break;
-      }
-    }
-  }
+   public ArrayUtils (ArrayUtils arrayUtils) {
+     this.arr = arrayUtils.arr.clone();
+   }
 
+   public void printArray () {
+     System.out.println(Arrays.toString(arr));
+   }
 
-  // task 2. Create a method that says Is the int array in ascending sequence.
-  static void arrayInAscendingOrder(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = i + 1; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-          int temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
+    // task 1
+    public void arrangeOddAndEven (){
+
+      int oddInd = 1;
+      int evenInd = 0;
+
+      while (true) {
+        while (evenInd < arr.length && arr[evenInd] % 2 == 0) {
+          evenInd += 2;
+        }
+        while (oddInd < arr.length && arr[oddInd] % 2 == 1) {
+          oddInd += 2;
+        }
+
+        if (evenInd < arr.length && oddInd < arr.length) {
+          int temp = arr[evenInd];
+          arr[evenInd] = arr[oddInd];
+          arr[oddInd] = temp;
+        } else {
+          break;
         }
       }
     }
-  }
 
-  // task 3. Create a method that returns the arithmetic mean of given int array elements
-  public static int average(int[] arr) {
-    int x = arr[0];
-    for (int i = 1; i < arr.length; i++) {
-      x += arr[i];
+    // task 2. Create a method that says Is the int array in ascending sequence.
+    public void arrayInAscendingOrder (){
+      for (int i = 0; i < arr.length; i++) {
+        for (int j = i + 1; j < arr.length; j++) {
+          if (arr[i] > arr[j]) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+        }
+      }
     }
-    return x / arr.length;
-  }
+
+    // task 3. Create a method that returns the arithmetic mean of given int array elements
+    public double average (){
+      int sum = arr[0];
+      for (int i = 1; i < arr.length; i++) {
+        sum += arr[i];
+      }
+      return (double) sum / arr.length;
+    }
 
   /* task 4. Create a method that receives two int arrays and
             returns an array of only the unique elements.*/
-  static int[] arrayUnion(int[] a, int[] b) {
+    public void arrayUnion ( int[] arr2){
 
-    int length = a.length + b.length;
-    boolean bool;
+      int m = arr[arr.length - 1];
+      int n = arr2[arr2.length - 1];
+      int ans = 0;
 
-    for (int i = 0; i < a.length; i++) {
-      for (int j = 0; j < b.length; j++) {
-        if (a[i] == b[j]) {
-          length--;
-        }
-      }
-    }
-
-    int[] newArray = new int[length];
-    int k = 0;
-
-    for (int i = 0; i < a.length; i++) {
-      newArray[k] = a[i];
-      k++;
-    }
-
-    for (int i = 0; i < b.length; i++) {
-      bool = true;
-      for (int j = 0; j < a.length; j++) {
-        if (b[i] == a[j]) {
-          bool = false;
-          continue;
-        }
-      }
-      if (bool) {
-        newArray[k] = b[i];
-        k++;
-      }
-    }
-    return newArray;
-  }
-
-  // task 5. Create a method that receives a sorted array and returns an array in random order.
-
-  static void randomArrayShuffle(int[] arr) {
-    Random r = new Random();
-
-    for (int i = arr.length - 1; i > 0; i--) {
-      int j = r.nextInt(i + 1);
-      int temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-  }
-
-   /* task 6. Create a method that finds pairs of numbers whose sum is equal
-             to a given value and return array with these values. */
-
-  static int[] chkPair(int arr[], int x) {
-    int[] twoValue = new int[2];
-    for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = (i + 1); j < arr.length; j++) {
-        if (arr[i] + arr[j] == x) {
-          twoValue[0] = arr[i];
-          twoValue[1] = arr[j];
-        }
-      }
-    }
-    return twoValue;
-  }
-
-
- /* task 7. Create a method that finds and returns the most frequently
-             occurring element in an array */
-
-  static int mostFrequentValue(int[] arr) {
-    Arrays.sort(arr);
-
-    int maxCount = 1;
-    int result = arr[0];
-    int currCount = 1;
-
-    for (int i = 1; i < arr.length; i++) {
-      if (arr[i] == arr[i - 1]) {
-        currCount++;
+      if (m > n) {
+        ans = m;
       } else {
-        if (currCount > maxCount) {
-          maxCount = currCount;
-          result = arr[i - 1];
+        ans = n;
+      }
+      int[] newTable = new int[ans + 1];
+
+      System.out.print(arr[0] + " ");
+      int i1 = ++newTable[arr[0]];
+
+      for (int i = 1; i < arr.length; i++) {
+        if (arr[i] != arr[i - 1]) {
+          System.out.print(arr[i] + " ");
+          ++newTable[arr[i]];
         }
-        currCount = 1;
+      }
+
+      for (int j = 0; j < arr2.length; j++) {
+        if (newTable[arr2[j]] == 0) {
+          System.out.print(arr2[j] + " ");
+          ++newTable[arr2[j]];
+        }
+      }
+
+    }
+
+    // task 5. Create a method that receives a sorted array and returns an array in random order.
+    public void randomArrayShuffle ( ){
+      for (int i = arr.length - 1; i > 0; i--) {
+        int j = new Random().nextInt(i + 1);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
       }
     }
-    if (currCount > maxCount) {
-      maxCount = currCount;
-      result = arr[arr.length - 1];
+
+  /* task 6. Create a method that finds pairs of numbers whose sum is equal
+            to a given value and return array with these values. */
+    public int[] chkPair ( int x){
+      int[] twoValue = new int[2];
+      for (int i = 0; i < arr.length - 1; i++) {
+        for (int j = (i + 1); j < arr.length; j++) {
+          if (arr[i] + arr[j] == x) {
+            twoValue[0] = arr[i];
+            twoValue[1] = arr[j];
+          }
+        }
+      }
+      return twoValue;
     }
-    return result;
-  }
 
 
+  /* task 7. Create a method that finds and returns the most frequently
+              occurring element in an array */
+    public int mostFrequentValue (){
+
+      int count1;
+      int popular1;
+      int count2 = 0;
+      int popular2 = 0;
+
+      for (int i = 0; i < arr.length; i++) {
+        popular1 = arr[i];
+        count1 = 0;
+
+        for (int j = i + 1; j < arr.length; j++) {
+          if (popular1 == arr[j]) {
+            count1++;
+          }
+        }
+
+        if (count1 > count2) {
+          popular2 = popular1;
+          count2 = count1;
+        } else if (count1 == count2) {
+          popular2 = Math.min(popular2, popular1);
+        }
+      }
+
+      return popular2;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,35 @@
 import java.util.Arrays;
 
 public class Main {
-
   public static void main(String[] args) {
 
-    final int[] arr1 = new int[]{1, 22, 32, 95, 22, 8, 73, 4, 51, 61};
-    final int[] arr2 = new int[]{5, 6, 15, 16, 20, 4, 61};
+    int[] arr1 = new int[]{1, 2, 4, 6, 5, 8, 9, 7, 4, 1, 1, 1};
+    int[] arr2 = new int[]{5, 6, 15, 16, 20, 4, 61};
+    ArrayUtils arrayUtils = new ArrayUtils(arr1);
 
     // task 1
-    ArrayUtils.arrangeOddAndEven(arr1);
-    System.out.println("task.1 \n" + Arrays.toString(arr1));
-
+    arrayUtils.arrangeOddAndEven();
+    System.out.println("task.1 ");
+    arrayUtils.printArray();
     // task 2
-    ArrayUtils.arrayInAscendingOrder(arr1);
-    System.out.println("task.2 \n" + Arrays.toString(arr1));
-
+    arrayUtils.arrayInAscendingOrder();
+    System.out.println("task.2 ");
+    arrayUtils.printArray();
     // task 3
-    System.out.println("task.3 \n" + ArrayUtils.average(arr1));
-
+    System.out.println("task.3 \n" + arrayUtils.average());
     // task 4
-    System.out.println("task.4 \n" + Arrays.toString(ArrayUtils.arrayUnion(arr1, arr2)));
-
+    System.out.println("task.4 ");
+    arrayUtils.arrayUnion(arr2);
+    System.out.println();
     // task 5
-    ArrayUtils.randomArrayShuffle(arr1);
-    System.out.println("task.5 \n" + Arrays.toString(arr1));
-
+    System.out.println("task.5");
+    arrayUtils.randomArrayShuffle();
+    arrayUtils.printArray();
     // task 6
-    System.out.println("task.6 \n" + Arrays.toString(ArrayUtils.chkPair(arr1, 9)));
-
+    System.out.println("task.6 \n" + Arrays.toString(arrayUtils.chkPair(9)));
     // task 7
-    System.out.println("task.7 \n" + ArrayUtils.mostFrequentValue(arr1));
+    System.out.println("task.7 \n" + arrayUtils.mostFrequentValue());
 
-
-    
 
   }
 }
